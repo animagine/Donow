40594a80fd4cdbfbc77b8c4d5552037f961ffdfe
animagine <animagine@live.com>
2024-01-24 13:46:08 +0000

    added timestamps and date display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-// display current day's date.
+// display current day and date in header.
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+//Define an on-click listener for button
+$(".saveBtn").on("click", function () {
+    //get nearby values.
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //set items in local storage.
+    localStorage.setItem(time, text);
+})
