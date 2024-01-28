da65724a46579b27c3e4a2b95c8ba450d88c6e50
animagine <animagine@live.com>
2024-01-28 01:13:06 +0000

    removed unnecceary whitespace

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,41 @@
 // display current day and date in header.
-var todayDate = dayjs().format('dddd, MMM Do YYYY');
+var todayDate = dayjs().format("dddd, MMM Do YYYY");
 $("#currentDay").html(todayDate);
 
 //Define an on-click listener for button
 $(".saveBtn").on("click", function () {
-    //get nearby values.
-    console.log(this);
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+  //get nearby values.
+  console.log(this);
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-    //set items in local storage.
-    localStorage.setItem(time, text);
-})
+  //set items in local storage.
+  localStorage.setItem(time, text);
+});
 
 function timeTracker() {
-    //get current number of hours.
-    var timeNow = dayjs().hour();
+  //get current number of hours.
+  var timeNow = dayjs().hour();
 
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+  // loop over time blocks
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-        // To check the time and add the classes for background indicators
-        if (blockTime < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (blockTime === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-
-        }
-    })
+    // To check the time and add the classes for background indicators
+    if (blockTime < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
 }
 
 //Retrieve any saved data from LocalStorage
@@ -52,6 +49,5 @@ $("#3PM .description").val(localStorage.getItem("3PM"));
 $("#4PM .description").val(localStorage.getItem("4PM"));
 $("#5PM .description").val(localStorage.getItem("5PM"));
 
-
-// calling the timeTracker function 
+// calling the timeTracker function
 timeTracker();
