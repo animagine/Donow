5760831204e3156ceff53b51de7d62f2b14cdf51
animagine <animagine@live.com>
2024-01-28 00:58:55 +0000

    fixed syntax error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // display current day and date in header.
-var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayDate = dayjs().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
 //Define an on-click listener for button
@@ -12,13 +12,44 @@ $(".saveBtn").on("click", function () {
     //set items in local storage.
     localStorage.setItem(time, text);
 })
+
+function timeTracker() {
+    //get current number of hours.
+    var timeNow = dayjs().hour();
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
 //Retrieve any saved data from LocalStorage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#9AM .description").val(localStorage.getItem("9AM"));
+$("#10AM .description").val(localStorage.getItem("10AM"));
+$("#11AM .description").val(localStorage.getItem("11AM"));
+$("#12PM .description").val(localStorage.getItem("12PM"));
+$("#1PM .description").val(localStorage.getItem("1PM"));
+$("#2PM .description").val(localStorage.getItem("2PM"));
+$("#3PM .description").val(localStorage.getItem("3PM"));
+$("#4PM .description").val(localStorage.getItem("4PM"));
+$("#5PM .description").val(localStorage.getItem("5PM"));
+
+timeTracker();
